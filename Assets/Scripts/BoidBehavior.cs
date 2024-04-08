c03d208667b0fe26da87a9663671bff180464d19
aesparza84 <aesparza@colum.edu>
2024-04-07 22:27:13 -0500

    Continuing boid behavior

--- Assets/Scripts/BoidBehavior.cs
+++ Assets/Scripts/BoidBehavior.cs
@@ -8,7 +8,7 @@ public class BoidBehavior : MonoBehaviour
     private Vector3 Direction;
 
     //Velocity vector
-    private Vector3 Velocity;
+    public Vector3 Velocity;
 
     //The target positon for ALL boids
     private Transform TargetObject;
@@ -16,15 +16,28 @@ public class BoidBehavior : MonoBehaviour
     //Flight speed of the boid
     [SerializeField] private float Speed;
 
-    private const float RotationSmoothing = 1.0f;
-    private const float DetectingRadius = 2.5f;
+    private const float LocalRadius = 10.0f;
+    private const float MinClumpDistance = 2.0f;
 
     //Buffer for checking neighboring Boids
     [SerializeField] private int MaxNeighbors;
     private Collider[] Neighboids;
+    private Collider myCollider;
+
+    [Range(0, 10)]
+    [SerializeField] private int DetectionRays;
+
+    private const int BitMask = (1 << 0);
 
     //Toggle for boids to follow/NOT Follow 'center' point
     public bool FreeFly;
+
+    //Rules 
+    public bool Alignment;
+    public bool Seperation;
+    public bool Cohesion;
+
+    [SerializeField] private bool SelfUpdate;
     private void Start()
     {
         //Setting the boid buffer
@@ -32,49 +45,142 @@ public class BoidBehavior : MonoBehaviour
 
         //Find Target position
         TargetObject = GameObject.Find("Target").transform;
+
+        myCollider = GetComponent<Collider>();
     }
 
+    private void Update()
+    {
+        if (SelfUpdate)
+        {
+            CalculateMovement();
+        }
+    }
     public void CalculateMovement()
     {
         //See nearby Boids
         //Set direction to avoid neighboids
         Direction = GetValidDirection();
 
-        transform.forward = Direction;
+        Quaternion newLook = Quaternion.LookRotation(Direction, transform.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, newLook, Time.deltaTime * 100.0f);
+
+        //Velocity = Direction.normalized * Speed * Time.deltaTime;
 
         //Update transform position
-        transform.position += Direction.normalized * Speed * Time.deltaTime;
+        transform.position += transform.forward * Speed * Time.deltaTime;
     }
 
     private Vector3 GetValidDirection()
     {
-        Vector3 newDirection = Vector3.zero;
-
-        //Check for nearby boids
-        //Get average of all nearby boids Vector
-        int neighboids = Physics.OverlapSphereNonAlloc(transform.position, DetectingRadius, Neighboids);
-        
-        for (int i = 0; i < neighboids; i++)
-        {
-            newDirection += (transform.position - Neighboids[i].transform.position);
-        }
+        Vector3 AvgHeading = Vector3.zero;
+        Vector3 AvgCenterPosition = Vector3.zero;
+        Vector3 SpacingVector = Vector3.zero;
+        Vector3 finalDirection = transform.forward;
 
         //ADD the desired 'center direction' to the direction
         if (!FreeFly && TargetObject != null)
         {
-            newDirection += (transform.position - TargetObject.position);
+            AvgHeading += (transform.position - TargetObject.position);
         }
 
-        //Average Direction of surrounding neighbors 
-        newDirection /= neighboids;
+        //Check for nearby boids
+        //
+        //Get average of all nearby boids Vector-HEADINGS
+        int neighboids = Physics.OverlapSphereNonAlloc(transform.position, LocalRadius, Neighboids);
+        
+        for (int i = 0; i < neighboids; i++)
+        {
+            if (Neighboids[i] != myCollider)
+            {
+                //Boids will try and aim towards center of neighbor cluster
+                if (Cohesion)
+                {
+                    AvgCenterPosition += Neighboids[i].transform.position;
+                }
+
+                //Boids align with neighboring headings
+                if (Alignment)
+                {
+                    AvgHeading += Neighboids[i].transform.forward;
+                }
+
+                //Boids will not collide with eachother
+                if (Seperation)
+                {
+                    float dist = Vector3.Distance(transform.position, Neighboids[i].transform.position);
+
+                    if (dist < MinClumpDistance)
+                    {
+                        //Direction TO index boid
+                        SpacingVector += (transform.position - Neighboids[i].transform.position);
+                    }
+                }
+            }
+            /*
+            if (Neighboids[i] != this)
+            {
+                //Avoids boids that are in front of boid
+                if (Vector3.Dot(transform.forward, Neighboids[i].transform.position) > 0.0f)
+                {
+                    newDirection += (transform.position - Neighboids[i].transform.position);
+                }
+            } 
+            */
+        }
+
+        //Average Direction of surrounding neighbors. HEADING
+        if (Alignment)
+        {
+            if (AvgHeading != Vector3.zero)
+            {
+                AvgHeading /= neighboids;
+            }
+            else
+            {
+                float ex = UnityEngine.Random.Range(-360, 360);
+                float ey = UnityEngine.Random.Range(-360, 360);
+                float ez = UnityEngine.Random.Range(-360, 360);
+                AvgHeading = new Vector3(ex, ey, ez);
+            }
+        }
+
+        //Averages the seperation vector
+        if (Seperation)
+        {
+            //Average of all directions from Boid to neighbor
+            if (SpacingVector != Vector3.zero)
+            {
+                SpacingVector /= neighboids;
+
+                //Make it negative to steer away from neighbors
+                SpacingVector *= -1;
+            }
+        }
+
+        //Averages position to get direction to 'center' of cluster
+        if (Cohesion)
+        {
+            if (AvgCenterPosition != Vector3.zero)
+            {
+                AvgCenterPosition /= neighboids;
+            }
+        }       
         
 
-        return newDirection.normalized;
-    }
+        //Vector3 finalDirection = (transform.position - AvgCenterPosition) + AvgHeading + SpacingVector; 
+        finalDirection += SpacingVector;
 
+        Debug.DrawRay(transform.position, finalDirection.normalized, Color.red);
+        
+        return finalDirection.normalized;
+    }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, DetectingRadius);
+        Gizmos.DrawWireSphere(transform.position, LocalRadius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, MinClumpDistance);
     }
 }
--- Assets/Scripts/SceneBoids.cs
+++ Assets/Scripts/SceneBoids.cs
@@ -11,6 +11,11 @@ public class SceneBoids : MonoBehaviour
 
     private const float SpawnOffset = 4.0f;
 
+    [SerializeField] private bool FreeFly;
+    [SerializeField] private bool Cohesion;
+    [SerializeField] private bool Alignment;
+    [SerializeField] private bool Seperation;
+
     private void Awake()
     {
         AllBoids = new List<BoidBehavior>();
@@ -22,7 +27,14 @@ public class SceneBoids : MonoBehaviour
             float z = UnityEngine.Random.Range(-SpawnOffset, SpawnOffset);
             Vector3 spawn = new Vector3(x, y, z);
 
-            GameObject m = Instantiate(BoidPrefab, transform.position + spawn, Quaternion.identity);
+            float ex = UnityEngine.Random.Range(-360, 360);
+            float ey = UnityEngine.Random.Range(-360, 360);
+            float ez = UnityEngine.Random.Range(-360, 360);
+
+            Vector3 rand = new Vector3(ex, ey, ez);
+            Quaternion spawnRot = Quaternion.Euler(rand);
+
+            GameObject m = Instantiate(BoidPrefab, transform.position + spawn, spawnRot);
             BoidBehavior mm = m.GetComponent<BoidBehavior>();
             AllBoids.Add(mm);
         }
@@ -37,7 +49,51 @@ public class SceneBoids : MonoBehaviour
     {
         foreach (BoidBehavior boid in AllBoids)
         {
+            boid.FreeFly = FreeFly;
+            boid.Alignment = Alignment;
+            boid.Cohesion = Cohesion;
+            boid.Seperation = Seperation;
             boid.CalculateMovement();
         }
+
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ResetAllBoids();
+        }
+    }
+
+    private void ResetAllBoids()
+    {
+        foreach (BoidBehavior item in AllBoids)
+        {
+            Destroy(item.gameObject);
+        }
+
+        AllBoids.Clear();
+
+        for (int i = 0; i < BoidAmount; i++)
+        {
+            float x = UnityEngine.Random.Range(-SpawnOffset, SpawnOffset);
+            float y = UnityEngine.Random.Range(-SpawnOffset, SpawnOffset);
+            float z = UnityEngine.Random.Range(-SpawnOffset, SpawnOffset);
+            Vector3 spawn = new Vector3(x, y, z);
+
+            float ex = UnityEngine.Random.Range(-360, 360);
+            float ey = UnityEngine.Random.Range(-360, 360);
+            float ez = UnityEngine.Random.Range(-360, 360);
+
+            Vector3 rand = new Vector3(ex, ey, ez);
+            Quaternion spawnRot = Quaternion.Euler(rand);
+
+            GameObject m = Instantiate(BoidPrefab, transform.position + spawn, spawnRot);
+            BoidBehavior mm = m.GetComponent<BoidBehavior>();
+            AllBoids.Add(mm);
+        }
+
     }
 }
